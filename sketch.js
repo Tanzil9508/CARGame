7b4ec13f7d007d1a603b3c3522dd34de964f3190
Tanzil9508 <74143093+Tanzil9508@users.noreply.github.com>
2020-12-24 19:55:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var car,wall;
+var speed,weight;
+
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+speed=random(90,50);
+weight=random(400,1500);
+ car  =  createSprite(50, 200, 50, 50);
+ car.shapeColor=("225");
+ car.velocityX =speed;
+ 
+
+wall = createSprite(1300,200,60,200);
+wall.shapeColor=("gray");
+
+
+
+}
+
+function draw() {
+  background(0);
+   if(wall.x-car.x < (car.weight+wall.weight)/2)
+    {
+     car.velocityX=0;
+     var deformation =0.5 * weight * speed * speed/22509;
+     if(deformation>180)
+     {
+       car.shapeColor=color("'225,0,0");
+     }
+     if(deformation<180 && deformation>100)
+     {
+       car.shapeColor=color("230,230,0")
+     }
+     if(deformation<100)
+     {
+       car.shapeColor=color("0,255,0")
+     }
+   }
+
+     if(car.isTouching(wall)){
+       car.velocityX=0;
+     }
+    
+     if(car.isTouching(wall)){
+       car.shapeColor=("purple");
+     }
+    
+  drawSprites();
+}
+
+
